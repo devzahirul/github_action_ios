35304c3ca8f94085552999fc3a3d486f301925c9
Zahirul_curlybracers <zahirulislam@curlybracers.com>
2022-06-02 05:51:24 +0600

    init project

---
+++ GithubAction-iOS/GithubAction_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAction_iOSApp.swift
+//  GithubAction-iOS
+//
+//  Created by Islam Md. Zahirul on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubAction_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
